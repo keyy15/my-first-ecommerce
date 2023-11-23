1477d3d3d71408664924138b090b8db28fcaeb9a
keyy15 <smeyhem01@gmail.com>
2023-11-23 14:01:57 +0700

    first commit

---
+++ src/components/TopNav.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const TopNav = () => {
+  return (
+    <div className='flex items-center justify-center'>
+      <div>1</div>
+      <div>2</div>
+    </div>
+  );
+}
+
+export default TopNav;
